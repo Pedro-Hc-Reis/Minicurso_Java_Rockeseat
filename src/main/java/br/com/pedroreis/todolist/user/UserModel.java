e62a188182ccb44ecea0745e0d2263cbdbeb1337
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2024-12-11 11:07:37 -0300

    Update UserModel.java

--- src/main/java/br/com/pedroreis/todolist/user/UserModel.java
+++ src/main/java/br/com/pedroreis/todolist/user/UserModel.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -25,4 +26,6 @@ public class UserModel {
 
     @CreationTimestamp
     private LocalDateTime createAt;
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
 }
