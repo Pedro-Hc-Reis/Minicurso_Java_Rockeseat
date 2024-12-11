5fd48dcaf863a91f25cd00b5158949216eefab32
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2024-12-11 14:16:47 -0300

    Update FilterTaskAuth.java

--- src/main/java/br/com/pedroreis/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/pedroreis/todolist/filter/FilterTaskAuth.java
@@ -1,17 +1,37 @@
 package br.com.pedroreis.todolist.filter;
 
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
     @Override
-    public void doFilter ( ServletRequest servletRequest , ServletResponse servletResponse , FilterChain filterChain ) throws IOException, ServletException {
+    protected void doFilterInternal ( HttpServletRequest request , HttpServletResponse response , FilterChain filterChain ) throws ServletException, IOException {
 
 
-        filterChain.doFilter ( servletRequest , servletResponse );
+        var authorization = request.getHeader ( "Authorization" );
+
+        var authEncoded = authorization.substring ( "Basic".length ( ) ).trim ( );
+
+        byte[] authDecode = Base64.getDecoder ().decode ( authorization );
+
+        var authString = new String ( authDecode );
+
+        String[] credentials = authString.split ( ":" );
+
+        String username = credentials[0];
+        String password = credentials[1];
+
+//        var user_password = authorization.substring ( "Basic".length ( ) ).trim ( );
+
+        filterChain.doFilter ( request , response );
     }
 }
