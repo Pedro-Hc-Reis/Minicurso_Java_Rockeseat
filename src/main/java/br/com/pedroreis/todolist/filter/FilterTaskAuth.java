3ead143ba9ef702fc165dd93e4f08d9faedf59ac
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2024-12-11 13:46:37 -0300

    Create FilterTaskAuth.java

---
+++ src/main/java/br/com/pedroreis/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,17 @@
+package br.com.pedroreis.todolist.filter;
+
+import jakarta.servlet.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class FilterTaskAuth implements Filter {
+
+    @Override
+    public void doFilter ( ServletRequest servletRequest , ServletResponse servletResponse , FilterChain filterChain ) throws IOException, ServletException {
+
+
+        filterChain.doFilter ( servletRequest , servletResponse );
+    }
+}
